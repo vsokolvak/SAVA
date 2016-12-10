1d9a1af1042320139a3f1318053b36911c9b47d0
Vadim <v.sokolvak@ukr.net>
2016-12-10 11:44:11 +0200

    Create js.js

---
+++ js.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+    //sozdau json
+    function creatjson(coursecount, lectioncount){
+        var jsontext ="{";
+        for (var i = 1; i <= coursecount; i++){
+        jsontext += "\"curse" + i + "\": [\"curse" + i + "\"";
+            for (var j = 1; j <= lectioncount; j++){
+                jsontext += ", \"lection" + j + "\"";
+            };
+            jsontext += "]";
+            if (i < coursecount)
+                jsontext += ", ";
+    
+        };
+        jsontext += "}"
+        return jsontext;
+    };
+    console.log(creatjson(2, 2));
+    
+    //
+    curs = JSON.parse(creatjson(2, 2));
+    console.dir(curs);
+    
+}
